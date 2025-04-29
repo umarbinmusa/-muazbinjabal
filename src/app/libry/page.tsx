ccead8885cc1df81ce2ab7e9943c761969754c10
umarbinmusa <umarmusa20002@gmail.com>
2025-04-29 14:37:18 +0100

    done with e-libry client

--- src/app/libry/page.tsx
+++ src/app/libry/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Footer from "../component/Footer";
 import Nav from "../Nav";
 function Libry (){
@@ -5,66 +6,89 @@ function Libry (){
         <div className="">
             <div className=""> <Nav /></div>
             
-            <div className="grid grid-cols-4 gap-4 pt-40">
-                <div className=" bg-white shadow-md p-10  font-black text-center">
+            <div className="grid lg:grid-cols-4 gap-4 pt-40 p-5">
+                <div className=" bg-white shadow-md p-10  font-black text-center border-2">
                    <h1>Riyadu Salihin</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white bg-black hover:bg-green-300 rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Kitabu tajweed</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Raddil Azha</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center ">
                    <h1>Fizilalul Quran </h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Safawatu tafasir</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black  text-center">
                    <h1>tafsirul jallalaini</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Muwadda malik</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Sahih Bukhari</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Sahih Muslim</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
-                <div className=" bg-white shadow-md p-10  font-black text-center">
+               <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Riwayatu Warash</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Riwayatu Qalun</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
                 <div className=" bg-white shadow-md p-10  font-black text-center">
                    <h1>Riwayatu Hafs</h1>
                    <h1>complete</h1>
                    <h1>volume</h1>
+                   <Link href="https://docs.google.com/document/d/1c7Y7cstVQb7MymPk6G1VnvbNlaxLGess/edit?usp=drive_link&ouid=109387066301936213225&rtpof=true&sd=true"><h2 className="text-white hover:bg-green-300 bg-black rounded-md">Read</h2> </Link>
+                
                 </div>
 
             </div>
