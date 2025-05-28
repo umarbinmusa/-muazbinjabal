834d030ee4219b63f139a96acbc41f95375f39d8
umarbinmusa <umarmusa20002@gmail.com>
2025-05-28 23:08:28 +0100

    ddd

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -62,7 +62,7 @@ function Login() {
       </div>
     </div>
     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
         Zip
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210"/>
